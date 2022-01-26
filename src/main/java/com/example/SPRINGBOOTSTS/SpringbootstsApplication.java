74531e5115954f3f253687bc73cdf4e7c8ec750c
karunakarmedam <98023012+karunakarmedam@users.noreply.github.com>
2022-01-26 10:36:54 +0530

    initial commit

---
+++ src/main/java/com/example/SPRINGBOOTSTS/SpringbootstsApplication.java
@@ -0,0 +1,13 @@
+package com.example.SPRINGBOOTSTS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootstsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootstsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SPRINGBOOTSTS/SpringbootstsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SPRINGBOOTSTS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootstsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
