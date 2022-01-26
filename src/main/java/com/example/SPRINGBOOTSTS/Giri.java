bf4d3fa091666fa10acc06d8271989a8f6276d75
karunakarmedam <98023012+karunakarmedam@users.noreply.github.com>
2022-01-26 10:58:59 +0530

    c

---
+++ src/main/java/com/example/SPRINGBOOTSTS/Giri.java
@@ -0,0 +1,10 @@
+package com.example.SPRINGBOOTSTS;
+
+public class Giri {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("------.Test");
+	}
+
+}
